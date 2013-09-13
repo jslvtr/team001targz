d77d7b73d0aad0d2b5deccdcf85926995516f13a
jslvtr <jslvtr@gmail.com>
2013-09-13 01:04:21 +0100

    Changed over to storing commands in text file

--- VideoEncoder/Ffmpeg.java
+++ VideoEncoder/Ffmpeg.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
@@ -7,6 +8,9 @@ import java.util.logging.Logger;
 
 
 public class Ffmpeg {
+	
+	private static final String fileinfo = "fileinfo.txt";
+	private static FileWriter writer;
 	
 	/**
 	 * Returns a string containing the file
@@ -16,7 +20,28 @@ public class Ffmpeg {
 	public String getFileInfo(File file) {
 		// RUN `ffmpeg -i <file>
 		String cmd = "ffmpeg -i " + file.getAbsolutePath();
-                //System.out.println(cmd);
+		File f = new File(fileinfo);
+		try {
+			f.createNewFile();
+			writer = new FileWriter(fileinfo);
+		} catch (Exception e) {
+			System.out.println("Error creating commands file");
+			e.printStackTrace();
+		}
+		
+		try {
+			writer.write(cmd);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				System.out.println("Error closing the File Writer.");
+				e.printStackTrace();
+			}
+		}
+        //System.out.println(cmd);
 		return _system(cmd);
 	}
 	
@@ -29,40 +54,40 @@ public class Ffmpeg {
 	 * @return true if success on conversion, false otherwise.
 	 */
 	public boolean convertFile(File src, File dest) {
-                //Get and parse video info
-                String[] info = getFileInfo(src).split("\n");
-                Parser parser = new Parser(info);
-                String previousType;
-                try {
-                        previousType = parser.getVideoType();
-                } catch (Exception ex) {
-                        System.out.println(ex.getMessage());
-                        return false;
-                }
+        //Get and parse video info
+        String[] info = getFileInfo(src).split("\n");
+        Parser parser = new Parser(info);
+        String previousType;
+        try {
+            previousType = parser.getVideoType();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
                 
                 //Record the starting time
 		int startTime = (int) (System.currentTimeMillis() / 1000L);
 		
-                //Run the convert command
-                String cmd = "ffmpeg -i " + src.getAbsolutePath() + " -vcodec libx264 -acodec libfdk_aac -vb 2500000 -ab 192000 -preset fast " + dest.getAbsolutePath();
-                _system(cmd);
-                
-                //Record the ending time
-                int endTime = (int) (System.currentTimeMillis() / 1000L);
-                
-                //Calculate encode time
-                int encodeTime = endTime - startTime;
-                try {
-                        //Create video object
-                        Video video = new Video(dest.getAbsolutePath(), parser.getDuration(), previousType, encodeTime);
-                } catch (Exception ex) {
-                        //An error occured, tell the user
-                        System.out.println(ex.getMessage());
-                        return false;
-                }
-                
-                //Add to queue
-                VideosEncoded ve = new VideosEncoded();
+        //Run the convert command
+        String cmd = "ffmpeg -i " + src.getAbsolutePath() + " -vcodec libx264 -acodec libfdk_aac -vb 2500000 -ab 192000 -preset fast " + dest.getAbsolutePath();
+        _system(cmd);
+        
+        //Record the ending time
+        int endTime = (int) (System.currentTimeMillis() / 1000L);
+        
+        //Calculate encode time
+        int encodeTime = endTime - startTime;
+        try {
+            //Create video object
+            Video video = new Video(dest.getAbsolutePath(), parser.getDuration(), previousType, encodeTime);
+        } catch (Exception ex) {
+            //An error occured, tell the user
+            System.out.println(ex.getMessage());
+            return false;
+        }
+        
+        //Add to queue
+        VideosEncoded ve = new VideosEncoded();
                 
 		
 		//END TIMER (RESULT IN MS)
@@ -70,22 +95,27 @@ public class Ffmpeg {
 	}
         
         private String _system(String cmd) {
-                String output = "";
-                try {
-                	Process p = new ProcessBuilder(cmd).start();
-                        p.waitFor();
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                        String line=reader.readLine();
+            String output = "";
+            try {
+            	ProcessBuilder pb = new ProcessBuilder();
+            	
+            	File commands = new File(fileinfo);
+            	pb.redirectInput(commands);
+            	Process p = pb.start();
+            	
+                p.waitFor();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line=reader.readLine();
 
-                        while (line != null) {
-				System.out.println(line);
-                            output += line + "\n";
-                            line = reader.readLine();
-                        }
+                while (line != null) {
+                	System.out.println(line);
+                    output += line + "\n";
+                    line = reader.readLine();
                 }
-                catch(IOException ex) { ex.printStackTrace(); }
-                catch(InterruptedException ex) { ex.printStackTrace(); }
-                
-                return output;
+            }
+            catch(IOException ex) { ex.printStackTrace(); }
+            catch(InterruptedException ex) { ex.printStackTrace(); }
+            
+            return output;
         }
 }
